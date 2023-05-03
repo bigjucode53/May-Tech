fff3d00ab337965d37a32e46301c0cf9f9d9138a
Julien Le Tiec <jletiec@gmail.com>
2023-05-03 10:40:40 +0200

    anim offre

--- js/bullenew.js
+++ js/bullenew.js
@@ -1,6 +1,26 @@
 var bulle10 = document.getElementById('bulle10');
-var bulle3 = document.getElementById('bulle11');
-// var bulle5 = document.getElementById('bulle5');
+
+
+var animationInProgress = {};
+
+var observer = new IntersectionObserver(function (entries, observer) {
+  entries.forEach(function (entry) {
+    if (entry.isIntersecting && !animationInProgress[entry.target.id]) {
+      entry.target.classList.add('active2');
+      animationInProgress[entry.target.id] = true;
+    } else if (!entry.isIntersecting && animationInProgress[entry.target.id]) {
+      entry.target.classList.remove('active2');
+      delete animationInProgress[entry.target.id];
+    }
+  });
+}, { threshold: 0 });
+
+observer.observe(bulle10);
+
+
+
+var bulle11 = document.getElementById('bulle11');
+
 var animationInProgress = {};
 
 var observer = new IntersectionObserver(function (entries, observer) {
@@ -15,6 +35,4 @@ var observer = new IntersectionObserver(function (entries, observer) {
   });
 }, { threshold: 0 });
 
-observer.observe(bulle10);
 observer.observe(bulle11);
-// observer.observe(bulle5);
